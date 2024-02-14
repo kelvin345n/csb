454dc819a92834d0236f3056228e456ab059e65a
kelvin345n <nkelvin1116@gmail.com>
2024-02-14 15:48:06 -0500

    Project 1b fin

---
+++ skeleton-sp23/proj1b/src/ArrayDeque.java
@@ -0,0 +1,182 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayDeque<T> implements Deque<T> {
+
+    private T[] arraydeque;
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+
+    /** Creates an Array of size 10 however no
+     * elements instantiated inside arraydeque */
+    public ArrayDeque(){
+        size = 0;
+        arraydeque = (T[])new Object[8];
+        nextFirst = 3;
+        nextLast = 4;
+    }
+    /**
+     * Add {@code x} to the front of the deque. Assumes {@code x} is never null.
+     * @param x item to add
+     */
+    @Override
+    public void addFirst(T x) {
+        resize_up_if_needed();
+        size += 1;
+        arraydeque[nextFirst] = x;
+        nextFirst = (nextFirst - 1 + arraydeque.length) % arraydeque.length;
+    }
+    /**
+     * Add {@code x} to the back of the deque. Assumes {@code x} is never null.
+     * @param x item to add
+     */
+    @Override
+    public void addLast(T x) {
+        resize_up_if_needed();
+        size += 1;
+        arraydeque[nextLast] = x;
+        nextLast = (nextLast + 1) % arraydeque.length;
+    }
+
+    private void resize_up_if_needed(){
+        /* Makes arraydeque two times longer if the original array is full */
+        if (size >= arraydeque.length){
+            T[] arraydeque_cpy = (T[])new Object[arraydeque.length*2];
+            int first = (nextFirst + 1) % arraydeque.length;
+            for (int i = 0; i < arraydeque.length; i++){
+                arraydeque_cpy[i] = arraydeque[(first) % arraydeque.length];
+                first = (first + 1) % arraydeque.length;
+            }
+            nextLast = arraydeque.length;
+            nextFirst = arraydeque_cpy.length - 1;
+            arraydeque = arraydeque_cpy;
+        }
+    }
+
+    /**
+     * Returns a List copy of the deque. Does not alter tne deque.
+     * @return a new list copy of the deque.
+     */
+    @Override
+    public List<T> toList() {
+        List<T> returnList = new ArrayList<>();
+        int first = (nextFirst + 1) % arraydeque.length;
+        for (int i = 0; i < size; i++){
+            returnList.add(arraydeque[(first) % arraydeque.length]);
+            first = (first + 1) % arraydeque.length;
+        }
+        return returnList;
+    }
+
+    /**
+     * Returns if the deque is empty. Does not alter the deque.
+     * @return {@code true} if the deque has no elements, {@code false} otherwise.
+     */
+    @Override
+    public boolean isEmpty() {
+        if (size == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Returns the size of the deque. Does not alter the deque.
+     * @return the number of items in the deque.
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Remove and return the element at the front of the deque, if it exists.
+     * @return removed element, otherwise {@code null}.
+     */
+    @Override
+    public T removeFirst() {
+        if (isEmpty()){
+            return null;
+        }
+        size -= 1;
+        T temp = arraydeque[(nextFirst + 1) % arraydeque.length];
+        arraydeque[(nextFirst + 1) % arraydeque.length] = null;
+        nextFirst = (nextFirst + 1) % arraydeque.length;
+        resize_down_if_needed();
+        return temp;
+    }
+
+    /**
+     * Remove and return the element at the back of the deque, if it exists.
+     * @return removed element, otherwise {@code null}.
+     */
+    @Override
+    public T removeLast() {
+        if (isEmpty()){
+            return null;
+        }
+        size -= 1;
+        T temp = arraydeque[(nextLast - 1) % arraydeque.length];
+        arraydeque[(nextLast - 1) % arraydeque.length] = null;
+        nextLast = (nextLast - 1) % arraydeque.length;
+        resize_down_if_needed();
+        return temp;
+    }
+    /** Only called in removeLast and removeFirst. Resizes if ratio
+     * is < 0.25 and array length greater than 9  */
+    private void resize_down_if_needed(){
+        /* If the ratio of items in arraydeque is less than or equal to
+        * 0.25, then we create a new arraydeque with half the original size
+        * unless the arraydeque has a length of 8 (Starting Length)*/
+        if (arraydeque.length <= 8){
+            return;
+        } else {
+            double ratio = (double)size / arraydeque.length;
+            if (ratio <= 0.25){
+                T[] arraydeque_cpy = (T[])new Object[arraydeque.length/2];
+                int first = (nextFirst + 1) % arraydeque.length;
+                for (int i = 0; i < size; i++){
+                    arraydeque_cpy[i] = arraydeque[(first) % arraydeque.length];
+                    first = (first + 1) % arraydeque.length;
+                }
+                nextLast = size;
+                nextFirst = arraydeque_cpy.length - 1;
+                arraydeque = arraydeque_cpy;
+            }
+        }
+    }
+
+    /**
+     * The Deque abstract data type does not typically have a get method,
+     * but we've included this extra operation to provide you with some
+     * extra programming practice. Gets the element. Does
+     * not alter the deque.
+     * @param index index to get, assumes valid index
+     * @return element at {@code index} in the deque
+     */
+    @Override
+    public T get(int index) {
+        if (isEmpty() || index < 0 || index >= size) {
+            return null;
+        } else {
+            int item = (nextFirst + 1 + index) % arraydeque.length;
+            return arraydeque[item];
+        }
+    }
+
+    public static void main(String[] args){
+        Deque<Integer> ad = new ArrayDeque<>();
+        ad.addFirst(13);
+        ad.addFirst(12);
+        ad.addFirst(11);
+        ad.addFirst(10);
+        ad.addFirst(9);
+        ad.addFirst(8);
+        ad.addLast(14);
+        ad.addLast(15);
+        ad.addLast(16);
+    }
+
+}
--- skeleton-sp23/proj1b/tests/ArrayDequeTest.java
+++ skeleton-sp23/proj1b/tests/ArrayDequeTest.java
@@ -1,8 +1,178 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDequeTest {
+    /** Tests the toList method in ArrayDeque */
+    @Test
+    public void test_toList(){
+        Deque<Integer> ad = new ArrayDeque<>();
+        ad.addFirst(13);
+        ad.addFirst(12);
+        ad.addFirst(11);
+        ad.addFirst(10);
+        ad.addFirst(9);
+        ad.addFirst(8);
+        ad.addLast(14);
+        List<Integer> actual = ad.toList();
+        List<Integer> expected = new ArrayList<>();
+        expected.add(8);
+        expected.add(9);
+        expected.add(10);
+        expected.add(11);
+        expected.add(12);
+        expected.add(13);
+        expected.add(14);
+        assertThat(actual).isEqualTo(expected);
+    }
 
+    /** Tests the isEmpty method in ArrayDeque */
+    @Test
+    public void test_isEmpty(){
+        Deque<String> ad = new ArrayDeque<>();
+        boolean actual = ad.isEmpty();
+        boolean expected = true;
+        assertThat(actual).isEqualTo(expected);
+
+        ad.addLast("Tongue");
+        ad.addFirst("Beach");
+        boolean actual1 = ad.isEmpty();
+        boolean expected1 = false;
+        assertThat(actual1).isEqualTo(expected1);
+    }
+
+    /** Tests the size method in ArrayDeque */
+    @Test
+    public void test_size(){
+        Deque<String> ad = new ArrayDeque<>();
+        int expected = 0;
+        int actual = ad.size();
+        assertThat(actual).isEqualTo(expected);
+
+        ad.addLast("Tongue");
+        int expected1 = 1;
+        int actual1 = ad.size();
+        assertThat(actual1).isEqualTo(expected1);
+
+        ad.addLast("Tongue");
+        ad.addLast("Tongue");
+        ad.addLast("Tongue");
+        ad.addLast("Tongue");
+        ad.addLast("Tongue");
+        ad.addLast("Tongue");
+        ad.addLast("Tongue");
+        ad.addLast("Tongue");
+        ad.addLast("Tongue");
+        int expected2 = 10;
+        int actual2 = ad.size();
+        assertThat(actual2).isEqualTo(expected2);
+    }
+
+    /** Tests the get method in ArrayDeque */
+    @Test
+    public void test_get(){
+        Deque<String> ad = new ArrayDeque<>();
+        String expected = null;
+        String actual = ad.get(0);
+        assertThat(actual).isEqualTo(expected);
+
+        ad.addFirst("Tongue");
+        ad.addFirst("Back");
+        ad.addFirst("Brain");
+        ad.addFirst("Heart");
+        ad.addFirst("Eyes");
+        ad.addFirst("Nose");
+        ad.addLast("Hair");
+        String expected1 = "Hair";
+        String actual1 = ad.get(6);
+        List<String> actual_list = ad.toList();
+        assertThat(actual_list).containsExactly("Nose", "Eyes", "Heart", "Brain",
+                                                        "Back", "Tongue", "Hair").inOrder();
+        assertThat(actual1).isEqualTo(expected1);
+
+        String expected2 = "Brain";
+        String actual2 = ad.get(3);
+        assertThat(actual2).isEqualTo(expected2);
+
+        String expected3 = null;
+        String actual3 = ad.get(7);
+        assertThat(actual3).isEqualTo(expected3);
+        actual_list = ad.toList();
+
+        assertThat(actual_list).containsExactly("Nose", "Eyes", "Heart", "Brain",
+                                                        "Back", "Tongue", "Hair").inOrder();
+
+        String expected4 = null;
+        String actual4 = ad.get(-1);
+        assertThat(actual4).isEqualTo(expected4);
+
+
+    }
+
+    /** Tests the removeFirst method in ArrayDeque */
+    @Test
+    public void test_removeFirst(){
+        Deque<Integer> ad = new ArrayDeque<>();
+        Integer actual = ad.removeFirst();
+        Integer expected = null;
+        assertThat(actual).isEqualTo(expected);
+
+        for (int i = 0; i < 10; i ++){
+            ad.addLast(5);
+        }
+
+        ad.addFirst(40);
+        int actual_size = ad.size();
+        int expected_size = 11;
+        assertThat(actual_size).isEqualTo(expected_size);
+
+        int actual1 = ad.removeFirst();
+        int expected1 = 40;
+        assertThat(actual1).isEqualTo(expected1);
+        assertThat(ad.removeFirst()).isEqualTo(5);
+
+        actual_size = ad.size();
+        expected_size = 9;
+        assertThat(actual_size).isEqualTo(expected_size);
+
+        List<Integer> actual_list = ad.toList();
+        assertThat(actual_list).containsExactly(5, 5, 5, 5, 5,
+                                                        5, 5, 5, 5);
+
+    }
+
+    /** Tests the removeLast method in ArrayDeque */
+    @Test
+    public void test_removeLast(){
+        Deque<Integer> ad = new ArrayDeque<>();
+        Integer actual = ad.removeLast();
+        Integer expected = null;
+        assertThat(actual).isEqualTo(expected);
+
+        for (int i = 0; i < 10; i ++){
+            ad.addLast(5);
+        }
+
+        ad.addLast(40);
+        int actual_size = ad.size();
+        int expected_size = 11;
+        assertThat(actual_size).isEqualTo(expected_size);
+
+        int actual1 = ad.removeLast();
+        int expected1 = 40;
+        assertThat(actual1).isEqualTo(expected1);
+        assertThat(ad.removeLast()).isEqualTo(5);
+
+        actual_size = ad.size();
+        expected_size = 9;
+        assertThat(actual_size).isEqualTo(expected_size);
+
+        List<Integer> actual_list = ad.toList();
+        assertThat(actual_list).containsExactly(5, 5, 5, 5, 5,
+                5, 5, 5, 5);
+    }
 }
