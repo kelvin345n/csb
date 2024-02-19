d1bbd274ed0689acc9ebda743d2f27b56512d813
kelvin345n <nkelvin1116@gmail.com>
2024-02-19 16:06:00 -0500

    Additional Tests Project 1C

--- skeleton-sp23/proj1c/src/deque/MaxArrayDeque.java
+++ skeleton-sp23/proj1c/src/deque/MaxArrayDeque.java
@@ -2,7 +2,7 @@ package deque;
 
 import java.util.Comparator;
 
-public class MaxArrayDeque<T> extends ArrayDeque<T>{
+public class MaxArrayDeque<T> extends ArrayDeque<T> {
     private Comparator<T> c;
     public MaxArrayDeque(Comparator<T> c){
         super();
@@ -27,4 +27,5 @@ public class MaxArrayDeque<T> extends ArrayDeque<T>{
     public T max(Comparator<T> c){
         return maxHelper(c);
     }
+
 }
--- skeleton-sp23/proj1c/tests/MaxArrayDequeTest.java
+++ skeleton-sp23/proj1c/tests/MaxArrayDequeTest.java
@@ -1,9 +1,9 @@
 import deque.MaxArrayDeque;
 import org.junit.jupiter.api.*;
 import deque.Deque;
-import deque.ArrayDeque;
 import deque.LinkedListDeque;
 
+import java.util.ArrayDeque;
 import java.util.Collection;
 import java.util.Comparator;
 
@@ -12,13 +12,20 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 public class MaxArrayDequeTest {
 
-    public class MaxComparator<T extends Comparable<T>> implements Comparator<T>{
+    public class LongestWordComparator<s extends String> implements Comparator<s>{
+        @Override
+        public int compare(s o1, s o2) {
+            return o1.length() - o2.length();
+        }
+    }
+
+    public class LastDigitComparator<Integer extends java.lang.Integer> implements Comparator<Integer>{
         /**
          * Compares its two arguments for order.  Returns a negative integer,
          * zero, or a positive integer as the first argument is less than, equal
          * to, or greater than the second.<p>
          * <p>
-         * The implementor must ensure that {@link Integer#signum
+         * The implementor must ensure that {@link java.lang.Integer#signum
          * signum}{@code (compare(x, y)) == -signum(compare(y, x))} for
          * all {@code x} and {@code y}.  (This implies that {@code
          * compare(x, y)} must throw an exception if and only if {@code
@@ -47,6 +54,13 @@ public class MaxArrayDequeTest {
          * this fact.  The recommended language is "Note: this comparator
          * imposes orderings that are inconsistent with equals."
          */
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o1.intValue() % 10 - o2.intValue() % 10;
+        }
+    }
+
+    public class MaxComparator<T extends Comparable<T>> implements Comparator<T>{
         @Override
         public int compare(T o1, T o2) {
             return o1.compareTo(o2);
@@ -54,43 +68,20 @@ public class MaxArrayDequeTest {
     }
 
     public class MinComparator<T extends Comparable<T>> implements Comparator<T>{
-        /**
-         * Compares its two arguments for order.  Returns a negative integer,
-         * zero, or a positive integer as the first argument is less than, equal
-         * to, or greater than the second.<p>
-         * <p>
-         * The implementor must ensure that {@link Integer#signum
-         * signum}{@code (compare(x, y)) == -signum(compare(y, x))} for
-         * all {@code x} and {@code y}.  (This implies that {@code
-         * compare(x, y)} must throw an exception if and only if {@code
-         * compare(y, x)} throws an exception.)<p>
-         * <p>
-         * The implementor must also ensure that the relation is transitive:
-         * {@code ((compare(x, y)>0) && (compare(y, z)>0))} implies
-         * {@code compare(x, z)>0}.<p>
-         * <p>
-         * Finally, the implementor must ensure that {@code compare(x,
-         * y)==0} implies that {@code signum(compare(x,
-         * z))==signum(compare(y, z))} for all {@code z}.
-         *
-         * @param o1 the first object to be compared.
-         * @param o2 the second object to be compared.
-         * @return a negative integer, zero, or a positive integer as the
-         * first argument is less than, equal to, or greater than the
-         * second.
-         * @throws NullPointerException if an argument is null and this
-         *                              comparator does not permit null arguments
-         * @throws ClassCastException   if the arguments' types prevent them from
-         *                              being compared by this comparator.
-         * @apiNote It is generally the case, but <i>not</i> strictly required that
-         * {@code (compare(x, y)==0) == (x.equals(y))}.  Generally speaking,
-         * any comparator that violates this condition should clearly indicate
-         * this fact.  The recommended language is "Note: this comparator
-         * imposes orderings that are inconsistent with equals."
-         */
         @Override
         public int compare(T o1, T o2) {
             return -(o1.compareTo(o2));
+        }
+    }
+
+    public class LongestWordAlphabeticalTiebreak <s extends String> implements Comparator<s>{
+        @Override
+        public int compare(s o1, s o2) {
+            if (o1.length() - o2.length() == 0){
+                return -(o1.compareTo(o2));
+            } else {
+                return o1.length() - o2.length();
+            }
         }
     }
 
@@ -138,4 +129,45 @@ public class MaxArrayDequeTest {
     // If and only if the Dog class implement Comparables<Dog>
     // Dog[] dogs = new Dog[]{d1, d2, d3};
     // Dog largest = Collection.max(dogs);
+
+    @Test
+    public void lastDigitCompTest(){
+        LastDigitComparator<Integer> maxMod = new LastDigitComparator<>();
+        MaxArrayDeque<Integer> mad = new MaxArrayDeque<>(maxMod);
+        mad.addLast(31);
+        mad.addLast(49);
+        mad.addLast(53);
+        mad.addLast(55);
+        int actual1 = mad.max();
+        int expected = 49;
+        assertThat(actual1).isEqualTo(expected);
+    }
+
+    @Test
+    public void longestStringTest(){
+        LongestWordComparator<String> longS = new LongestWordComparator<>();
+        MaxArrayDeque<String> mad = new MaxArrayDeque<>(longS);
+        mad.addLast("Longest");
+        mad.addLast("Longestest");
+        mad.addLast("Longerer");
+        mad.addLast("Long");
+        String actual1 = mad.max();
+        String expected = "Longestest";
+        assertThat(actual1).isEqualTo(expected);
+    }
+
+    @Test
+    public void longestWordTieBreakAlphabeticalTest(){
+        LongestWordAlphabeticalTiebreak<String> longA = new LongestWordAlphabeticalTiebreak<>();
+        MaxArrayDeque<String> mad = new MaxArrayDeque<>(longA);
+        mad.addLast("Beach");
+        mad.addLast("Bet");
+        mad.addLast("Big");
+        mad.addLast("Beets");
+        mad.addLast("Teach");
+        String actual1 = mad.max();
+        String expected = "Beach";
+        assertThat(actual1).isEqualTo(expected);
+    }
+
 }
